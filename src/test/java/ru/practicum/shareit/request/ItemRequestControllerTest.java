a2c09953f8b335d8f91a7df3ac16f800b4cbe6fd
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2024-03-10 21:18:30 +0300

    test: Request Item

--- src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
@@ -12,16 +12,19 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.request.dto.ItemRequestCreatDto;
+import ru.practicum.shareit.request.dto.ItemRequestInfoDto;
 import ru.practicum.shareit.request.dto.ItemRequestReadDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
 
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -72,14 +75,93 @@ class ItemRequestControllerTest {
     }
 
     @Test
+    @SneakyThrows
+    @DisplayName("Получние всех requests по requester id")
     void getAllRequestsByRequesterId() {
+        Long requestId = 1L;
+        Long userId = 2L;
+        String description = "description";
+        ItemRequestInfoDto itemRequestInfoDto = ItemRequestInfoDto.builder()
+                .id(requestId)
+                .description(description)
+                .created(created)
+                .items(null)
+                .build();
+        List<ItemRequestInfoDto> infoDtoList = new ArrayList<>();
+        infoDtoList.add(itemRequestInfoDto);
+
+        when(itemRequestService.getAllRequestsByRequesterId(anyLong()))
+                .thenReturn(infoDtoList);
+
+        mockMvc.perform(get("/requests")
+                        .header(userIdHeader, userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id").value(itemRequestInfoDto.getId()))
+                .andExpect(jsonPath("$.[0].description").value(itemRequestInfoDto.getDescription()))
+                .andExpect(jsonPath("$.[0].created").value(itemRequestInfoDto.getCreated().format(pattern)))
+                .andExpect(jsonPath("$.[0].items").value(itemRequestInfoDto.getItems()));
+
+        Mockito.verify(itemRequestService, Mockito.times(1))
+                .getAllRequestsByRequesterId(anyLong());
     }
 
     @Test
+    @SneakyThrows
+    @DisplayName("Получние всех requests")
     void getAllRequests() {
+        Long requestId = 1L;
+        Long userId = 2L;
+        String description = "description";
+        ItemRequestInfoDto itemRequestInfoDto = ItemRequestInfoDto.builder()
+                .id(requestId)
+                .description(description)
+                .created(created)
+                .items(null)
+                .build();
+        List<ItemRequestInfoDto> infoDtoList = new ArrayList<>();
+        infoDtoList.add(itemRequestInfoDto);
+
+        when(itemRequestService.getAllRequestItem(anyLong(), anyInt(), anyInt()))
+                .thenReturn(infoDtoList);
+
+        mockMvc.perform(get("/requests/all")
+                        .header(userIdHeader, userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id").value(itemRequestInfoDto.getId()))
+                .andExpect(jsonPath("$.[0].description").value(itemRequestInfoDto.getDescription()))
+                .andExpect(jsonPath("$.[0].created").value(itemRequestInfoDto.getCreated().format(pattern)))
+                .andExpect(jsonPath("$.[0].items").value(itemRequestInfoDto.getItems()));
+
+        Mockito.verify(itemRequestService, Mockito.times(1))
+                .getAllRequestItem(anyLong(), anyInt(), anyInt());
     }
 
     @Test
+    @SneakyThrows
+    @DisplayName("Получение request по id")
     void getRequestByRequestId() {
+        Long requestId = 1L;
+        Long userId = 2L;
+        String description = "description";
+        ItemRequestInfoDto itemRequestInfoDto = ItemRequestInfoDto.builder()
+                .id(requestId)
+                .description(description)
+                .created(created)
+                .items(null)
+                .build();
+
+        when(itemRequestService.getItemRequestById(anyLong(), anyLong()))
+                .thenReturn(itemRequestInfoDto);
+
+        mockMvc.perform(get("/requests/{requestId}", requestId)
+                        .header(userIdHeader, userId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(itemRequestInfoDto.getId()))
+                .andExpect(jsonPath("$.description").value(itemRequestInfoDto.getDescription()))
+                .andExpect(jsonPath("$.created").value(itemRequestInfoDto.getCreated().format(pattern)))
+                .andExpect(jsonPath("$.items").value(itemRequestInfoDto.getItems()));
+
+        Mockito.verify(itemRequestService, Mockito.times(1))
+                .getItemRequestById(anyLong(), anyLong());
     }
 }
