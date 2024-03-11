7f1cdcadd67dc7b271b7c6763edfa2fa6561d534
Kim Bogdanov <105812215+KimBogdanov@users.noreply.github.com>
2024-03-11 22:58:22 +0300

    test: UserRepository

--- src/test/java/ru/practicum/shareit/user/repository/UserRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/user/repository/UserRepositoryTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
+
     @Test
     @DisplayName("Вернет true если email уже используется")
     public void testExistsByEmail() {
